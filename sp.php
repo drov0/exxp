0efd65dfd67a4690795602b8af00b461706ecba4
Martin Lees <drov0@users.noreply.github.com>
2018-01-22 17:37:10 +0100

    update the boostrap file

--- sp.php
+++ sp.php
@@ -13,8 +13,7 @@
  * @package           Sp
  *
  * @wordpress-plugin
- * Plugin Name:       Steempress
- * Plugin URI:        https://steemit.com/@howo
+ * Plugin Name:       SteemPress
  * Description:       Automatically share your content to steemit
  * Version:           1.0.0
  * Author:            Martin Lees
@@ -31,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
- * Currently pligin version.
+ * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */

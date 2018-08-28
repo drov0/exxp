7125bf73b082f4f4bff64fb847ace2015607839a
Martin Lees <drov0@users.noreply.github.com>
2018-08-28 02:53:20 +0200

    Small js rewrite

--- public/js/steempress_sp-public.js
+++ public/js/steempress_sp-public.js
@@ -26,32 +26,27 @@
         }
 
 
-        // Make the actual CORS request.
-        function makeCorsRequest() {
-            var url = "http://localhost:8002";
+		var url = "http://localhost:8002";
 
-            var xhr = createCORSRequest('POST', url);
-            var params = "username="+username+"&permlink="+permlink+"&tag="+tag;
-            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.setRequestHeader("Content-length", params.length);
-            if (!xhr) {
-                return;
-            }
+		var xhr = createCORSRequest('POST', url);
+		var params = "username="+username+"&permlink="+permlink+"&tag="+tag;
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.setRequestHeader("Content-length", params.length);
+		if (!xhr) {
+			return;
+		}
 
-            // Response handlers.
-            xhr.onload = function() {
-                var data = JSON.parse(xhr.responseText);
-                $("#steempress_sp_price")[0].innerHTML = data.payout;
-                $("#steempress_sp_comments")[0].innerHTML = data.comments;
-            };
+		// Response handlers.
+		xhr.onload = function() {
+			var data = JSON.parse(xhr.responseText);
+			$("#steempress_sp_price")[0].innerHTML = data.payout;
+			$("#steempress_sp_comments")[0].innerHTML = data.comments;
+		};
 
-            xhr.send(params);
-        }
+		xhr.send(params);
+	}
 
-        makeCorsRequest();
-    }
-
-    $(window).load(load_steem_capabilities)
+	$(window).load(load_steem_capabilities)
 
 })( jQuery );
 

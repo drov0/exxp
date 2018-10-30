865a0de2fa142fdf7a28c0d6dedbffed95d27d78
Martin Lees <drov0@users.noreply.github.com>
2018-10-30 16:16:14 +0100

    Disabled 'powered by steempress'  on the front page

--- steempress_sp.php
+++ steempress_sp.php
@@ -35,9 +35,9 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'steempress_sp_compte', '1.9.9');
 define( 'steempress_sp_api_url', 'https://api.steempress.io');
-define( 'steempress_sp_twoway_api_url', 'https://two.steempress.io');
+//define( 'steempress_sp_twoway_api_url', 'https://two.steempress.io');
 //define( 'api_url', 'http://localhost:8001');
-//define( 'steempress_sp_twoway_api_url', 'http://localhost:8002');
+define( 'steempress_sp_twoway_api_url', 'http://localhost:8002');
 
 /**
  * The code that runs during plugin activation.
